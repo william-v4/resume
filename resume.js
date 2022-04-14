4d0ed0915461123388390044085fc0744df7c78a
William Zhang <william.exe@posteo.net>
2022-04-13 23:32:09 -0600

    brain cancer

--- resume.js
+++ resume.js
@@ -1,3 +1,18 @@
 document.getElementById("call").onclick = function() {
     window.open("tel:william.exe@sip.linphone.org")
 }
+document.getElementById("body").onload = function() {
+    document.getElementById("side").style.animationName = "slide";
+    document.getElementById("body").style.animationName = "fadein";
+}
+var turningpoint = document.getElementById("xp").getBoundingClientRect();
+window.onscroll = function() {
+    while (turningpoint.top >= 0 && turningpoint.left >= 0 && turningpoint.right <= window.innerWidth && turningpoint.bottom <= window.innerHeight) {
+        var read = 1
+    }
+    if (read = 1) {
+        document.getElementById("bigchungus").style.animationName = "fadein";
+    } else {
+        document.getElementById("bigchungus").style.opacity = "0.9";
+    }
+}
